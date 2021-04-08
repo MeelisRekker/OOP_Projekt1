c7d29e05a56a9285040871d9cb9c1f78de2ec5ad
MeelisRekker <32929450+MeelisRekker@users.noreply.github.com>
2021-04-08 11:32:41 +0300

    Valma
    Final push

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Peaklass extends Poomine{
+public class Peaklass extends Poomine {
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("sõnad.txt"), StandardCharsets.UTF_8); //loeb mängu jaoks sõnad failist
-        Scanner klaviatuur = new Scanner(System.in); //loeb klaviatuurilt sisetatud asju
+        Scanner sc = new Scanner(new File("sõnad.txt"), StandardCharsets.UTF_8);        //loeb mängu jaoks sõnad failist
+        Scanner klaviatuur = new Scanner(System.in);                                       //loeb klaviatuurilt sisetatud asju
 
         List<String> sõnad = new ArrayList<>();
 
@@ -17,47 +17,42 @@ public class Peaklass extends Poomine{
             sõnad.add(sc.nextLine());
         }
 
-        Random rand = new Random(); //valib randomiga suvalise sõna sisseloetute seast
-
+        Random rand = new Random();                                               //valib randomiga suvalise sõna sisseloetute seast
         String sõna = sõnad.get(rand.nextInt(sõnad.size()));
-
-
         List<Character> mängijaArvamised = new ArrayList<>();
 
         int valesti = 0;
-        System.out.println("Tere tulemast mängima poomismängu! Sisestage tähti kuni arvate ära sõna või kaotate!");
-        while(true) {  //while-tsükkel kuni mäng katkeb
 
+
+        System.out.println("\nTere tulemast mängima poomismängu! Sisestage tähti kuni arvate ära sõna või kaotate!");
+
+        while(true) {                                       //while-tsükkel kuni mäng katkeb
             prindiMees(valesti);
 
             if (valesti >= 6) {
-                System.out.println("Kaotasid!");
-                System.out.println("Valitud sõna oli " + sõna);
+                System.out.println("Kaotasid! Valitud sõna oli " + ANSI_RED + sõna + ANSI_RESET + "!");
                 break;
             }
 
-            System.out.println("Arvatav sõna: ");
             prindiSõnaSeis(sõna, mängijaArvamised);
-            if (!getMängijaArvamised(klaviatuur, sõna, mängijaArvamised)) { //kui arvatud täht ei ole sõna osa, suurendab counterit
+
+            if (!getMängijaArvamised(klaviatuur, sõna, mängijaArvamised)) {         //kui arvatud täht ei ole sõna osa, suurendab counterit
                 valesti++;
             }
 
-
-            if(prindiSõnaSeis(sõna, mängijaArvamised)){ //kui sõna on arvatud (õigeid on sama palju nagu sõna pikkus), lõpetab mängu
+            if(prindiSõnaSeis(sõna, mängijaArvamised)){                     //kui sõna on arvatud (õigeid on sama palju nagu sõna pikkus), lõpetab mängu
                 System.out.println("Võitsid!");
                 break;
             }
-
-            System.out.println("Arva, mis sõna võiks olla: ");
+            System.out.print("Arva, mis sõna võiks olla: ");
             if (klaviatuur.nextLine().equals(sõna)){ //kui kirjutati õige sõna, lõpetab mängu
                 System.out.println("Võitsid!");
                 break;
             }
             else {
-                System.out.println("Arvasid valesti! Proovi edasi!"); //jätkab mängu, kui arvati valesti
+                System.out.println("Arvasid valesti! Proovi edasi!");
             }
+            System.out.println("Oled vale tähte pakkunud " + valesti + " korda");
         }
-
     }
-
 }
--- src/Poomine.java
+++ src/Poomine.java
@@ -1,58 +1,75 @@
 import java.util.List;
 import java.util.Scanner;
 
-public class Poomine {
+public class Poomine  {
 
-    static boolean prindiSõnaSeis(String sõna, List<Character> mängijaArvamised) { //meetod sõna arvamise seisu printimiseks
+    static String ANSI_RESET = "\u001B[0m";                        //konsooli teksti värvid
+    static String ANSI_BLACK = "\u001B[30m";
+    static String ANSI_RED = "\u001B[31m";
+    static String ANSI_GREEN = "\u001B[32m";
+    static String ANSI_BLUE = "\u001B[34m";
+
+
+    static boolean prindiSõnaSeis(String sõna, List<Character> mängijaArvamised) {          //meetod sõna arvamise seisu printimiseks
         int õigeid = 0;
-        for (int i = 0; i < sõna.length(); i++) { //prindib kas kriipsud või leitud tähed
-            if (mängijaArvamised.contains(sõna.charAt(i))) {
-                System.out.print(sõna.charAt(i));
-                õigeid++;
-            } else {
-                System.out.print("-");
-            }
+        System.out.print("Arvatav sõna:   ");
 
+        for (int i = 0; i < sõna.length(); i++) {                   //prindib kas kriipsud või leitud tähed
+            if (mängijaArvamised.contains(sõna.charAt(i))) {
+                System.out.print(ANSI_GREEN + sõna.charAt(i) + ANSI_RESET);
+                õigeid++;
+            } else
+                System.out.print(ANSI_RED + "_" + ANSI_RESET);
         }
         System.out.println();
-
-        return (sõna.length() == õigeid); //kas sõna on arvatud
-
+        return (sõna.length() == õigeid);               //kas sõna on arvatud
     }
 
-    static boolean getMängijaArvamised(Scanner klaviatuur, String sõna, List<Character> mängijaArvamised) { //meetod mängija tähtede sisestamiseks
-        System.out.println("Sisesta üks täht: ");
-        String arvatudTäht = klaviatuur.nextLine();
-        mängijaArvamised.add(arvatudTäht.charAt(0));  //kui mängija sisestab mingi pikema asja, loeb ainult esimese tähe
-        return sõna.contains(String.valueOf(arvatudTäht.charAt(0)));  //kas sõna sisaldab arvatud tähte
-    }
+    static boolean getMängijaArvamised(Scanner klaviatuur, String sõna, List<Character> mängijaArvamised) {     //meetod mängija tähtede sisestamiseks
+        System.out.print("Sisesta üks täht: ");
 
-    static void prindiMees(int valesti) { //meetod poodud mehikese printimiseks, lubatud on 5 vale arvamist
-        System.out.println(" -------");
-        System.out.println(" |");
-        if (valesti >= 1) {
-            System.out.println(" O");
+        String sisend = klaviatuur.nextLine();
+
+        if (sisend.equals("")) {
+            System.out.println(ANSI_BLUE + "jätkan.." + ANSI_RESET);
+            System.out.print("Sõna praegu:   ");
+            return false;
         }
+
+        Character uusTäht = sisend.charAt(0);
+
+        for (Character arvatudTähed : mängijaArvamised) {
+           if (arvatudTähed.equals(uusTäht))
+               System.out.println(ANSI_BLUE + "Tähte " + uusTäht + " juba arvati" + ANSI_RESET);
+        }
+        mängijaArvamised.add(uusTäht);                                    //kui mängija sisestab mingi pikema asja, loeb ainult esimese tähe
+        return sõna.contains(String.valueOf(uusTäht));                    //kas sõna sisaldab arvatud tähte
+
+    }
+
+    static void prindiMees(int valesti) {                               //meetod poodud mehikese printimiseks, lubatud on 5 vale arvamist
+        System.out.println(ANSI_BLACK + " _______" + ANSI_RESET);
+        System.out.println(ANSI_BLACK + " |" + ANSI_RESET);
+        if (valesti >= 1)
+            System.out.println(" O");
 
         if (valesti >= 2) {
             System.out.print("\\ ");
-            if (valesti >= 3) {
+            if (valesti >= 3)
                 System.out.println("/");
-            } else {
-                System.out.println("");
-            }
+            else
+                System.out.println();
         }
 
-        if (valesti >= 4) {
+        if (valesti >= 4)
             System.out.println(" |");
-        }
+
         if (valesti >= 5) {
             System.out.print("/ ");
-            if (valesti >= 6) {
+            if (valesti >= 6)
                 System.out.println("\\");
-            } else {
-                System.out.println("");
-            }
+            else
+                System.out.println();
         }
     }
 }
